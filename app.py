c8b9e4f0522d012933c2762bfadabc416073e455
Hyatt Kapoor <kapoorhyatt@gmail.com>
2026-04-07 10:03:09 +1000

    Your commit message here

--- app.py
+++ app.py
@@ -415,3 +415,7 @@ def assumptions():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # use Render's dynamic PORT
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)  # <- debug=True
