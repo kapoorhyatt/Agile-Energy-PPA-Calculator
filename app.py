0aba05bfcaaa9ccdec5dde8d03372dcccb925719
Hyatt Kapoor <kapoorhyatt@gmail.com>
2026-04-19 22:21:06 +1000

    your message here

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ def get_db_connection():
 app = Flask(__name__)
 app.secret_key = "dev-secret-key"
 
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE="Lax",
+    SESSION_PERMANENT=False
+)
 def link_callback(uri, rel):
     if uri.startswith('/static/'):
         return os.path.join(BASE_DIR, uri.lstrip('/'))
